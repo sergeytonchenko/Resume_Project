8418d7d9856f4cb23cea9a83fde7f6d8975f2d91
sergeytoncenko <randick56@gmail.com>
2020-02-07 00:05:33 +0200

    add

--- build/js/main.js
+++ build/js/main.js
@@ -12,24 +12,29 @@
 //   });
 
 $("#portfolio__slider").owlCarousel({
-    items: 3,
+    items: 1,
     loop: true,
     margin: 30,
     nav: true,
     autoplay: true,
     dotsEach: 1,                           
-    autoplayTimeout: 2000,
-    responsiveClass:true,
-    responsive:{
-        0 :{
-            items: 1,
-            nav: false
-        },
-        690 :{
-            items: 2
-        },
-        992 :{
-            items: 3
-        }
-    }
+    autoplayTimeout: 3000,
+    // responsiveClass:true,
+    animateIn: 'zoomInRight',
+    animateOut: "zoomOutLeft",
+    // responsive:{
+    //     0 :{
+    //         items: 1,
+    //         nav: false
+    //     },
+    //     690 :{
+    //         items: 2
+    //     },
+    //     992 :{
+    //         items: 3
+    //     }
+    // }
 });
+
+$('.owl-prev span').html('<img src="../img/portfolio/prev.png"></img>');
+$('.owl-next span').html('<img src="../img/portfolio/next.png"></img>');
