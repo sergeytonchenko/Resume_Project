a28f62edda150e8e325e2f7ee4ee8b8b1da1df92
sergeytoncenko <randick56@gmail.com>
2020-02-06 23:02:30 +0200

    add

--- build/js/main.js
+++ build/js/main.js
@@ -1,12 +1,12 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop()>=900) {
-      // длительность анимации - 'slow'
-      // тип анимации -  'linear'
-      $('.aboutme__block1').fadeIn('slow','linear');
-    }
-    else {
-      // длительность анимации - 'fast'
-      // тип анимации -  'swing'
-      $('.aboutme__block1').fadeOut('fast','swing');
-    }
-  });
+// $(window).scroll(function() {
+//     if ($(this).scrollTop()>=900) {
+//       // длительность анимации - 'slow'
+//       // тип анимации -  'linear'
+//       $('.aboutme__block1').fadeIn('slow','linear');
+//     }
+//     else {
+//       // длительность анимации - 'fast'
+//       // тип анимации -  'swing'
+//       $('.aboutme__block1').fadeOut('fast','swing');
+//     }
+//   });
