0c277b019596bcad153c7bdef68fbfdf74d6fa43
sergeytoncenko <randick56@gmail.com>
2020-02-06 23:20:11 +0200

    add

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,26 @@
 //       $('.aboutme__block1').fadeOut('fast','swing');
 //     }
 //   });
+
+$("#portfolio__slider").owlCarousel({
+    items: 3,
+    loop: true,
+    margin: 30,
+    nav: true,
+    autoplay: true,
+    dotsEach: 1,                           
+    autoplayTimeout: 2000,
+    responsiveClass:true,
+    responsive:{
+        0 :{
+            items: 1,
+            nav: false
+        },
+        690 :{
+            items: 2
+        },
+        992 :{
+            items: 3
+        }
+    }
+});
